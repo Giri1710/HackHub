32658e9084ea4b5c22345093f92363b8c39970fd
Giri L <lg8169425@gmail.com>
2026-09-25 10:04:45 +0530

    Fix Docker deployment and database connection

--- src/main/java/com/hackhub/util/DBConnection.java
+++ src/main/java/com/hackhub/util/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DBConnection {
 
     private static final String URL =
-            "jdbc:mysql://localhost:3306/hackhub";
+        "jdbc:mysql://host.docker.internal:3306/hackhub";
 
     private static final String USER =
             "root";
